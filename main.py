0b18f9e751339b5e36e2c98e0a940a9b87c6af4d
gmagby <gbratt9@gmail.com>
2026-01-07 23:00:17 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import tkinter as tk
+import wotd
+from wotd import WordVariant, list_of_word_variants
+
+NUM = len(list_of_word_variants)
+st.header("Word of the Day", divider="rainbow")
+st.subheader(wotd.WORD)
+
+
+st.markdown(
+    f'{list_of_word_variants[0].definition}'
+
+
+
+)
+
+st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
