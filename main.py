44b439222e113a969a8c0a52990abca296364910
gmagby <gbratt9@gmail.com>
2026-01-07 23:30:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ st.subheader(wotd.WORD)
 st.markdown(
     f'{list_of_word_variants[0].definition}')
 st.markdown(
-    f'{list_of_word_variants[0].type_of_speech}')
+    f'Type of Speech: {list_of_word_variants[0].type_of_speech}')
 st.markdown(
     f'Date first used: {list_of_word_variants[0].date}')
 st.markdown(
     f'{list_of_word_variants[0].etymology}')
 
 st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
+
 
 
 
