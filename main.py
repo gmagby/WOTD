4935f4470e2e18cdfa3a36f68f3f9562034478ec
gmagby <gbratt9@gmail.com>
2026-01-07 23:16:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ st.markdown(
     f'{list_of_word_variants[0].type_of_speech}'
     f'{list_of_word_variants[0].date}'
     f'{list_of_word_variants[0].etymology}'
-
-
-
 )
 
 st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
-
-
