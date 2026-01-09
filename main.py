854511523292649b1384cae09307b79032fa820c
gabriel <dvabnnzsngf>
2026-01-08 20:58:58 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def more_definitions():
         st.markdown(
             f'{list_of_word_variants[t].definition}')
         st.markdown(
-            f'Type of Speech: {list_of_word_variants[t].type_of_speech}')
+            f'Type of Speech: **{list_of_word_variants[t].type_of_speech}**')
         st.markdown(
-            f'Date first used: {list_of_word_variants[t].date}')
-        st.markdown(
-            f'{list_of_word_variants[t].etymology}')
+            f'Date first used: **{list_of_word_variants[t].date}**')
+        # st.markdown(
+        #     f'{list_of_word_variants[t].etymology}')
         st.header("",divider="rainbow")
 
 def first_definition():
@@ -28,9 +28,12 @@ def first_definition():
         f'Date first used: **{list_of_word_variants[0].date}**')
     # st.markdown(
     #     f'{list_of_word_variants[0].etymology}')
-    if st.button("More Definitions"):
-        more_definitions()
-first_definition()
+
+
+if not st.button("More Definitions"):
+    first_definition()
+else:
+    more_definitions()
 
 # st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
 # Open 'data/report.txt' for writing ('w')
