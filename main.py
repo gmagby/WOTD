4c34221b017f47caf73c26d1f55bd7aec4b8e73e
gabriel <dvabnnzsngf>
2026-01-08 21:12:30 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ def first_definition(On=True):
             f'Date first used: **{list_of_word_variants[0].date}**')
         # st.markdown(
         #     f'{list_of_word_variants[0].etymology}')
-    else:
-        return None
 
 first_definition(True)
 
