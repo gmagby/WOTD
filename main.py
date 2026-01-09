3e2cca41b5de7c8ca6a5090a835aa704eb9e00f9
gabriel <dvabnnzsngf>
2026-01-08 20:37:13 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ NUM = len(list_of_word_variants)
 st.header("Word of the Day", divider="rainbow")
 st.subheader(wotd.WORD)
 
+def more_definitions():
+    for t in list_of_word_variants:
+        st.markdown(
+            f'{list_of_word_variants[t].definition}')
+        st.markdown(
+            f'Type of Speech: {list_of_word_variants[t].type_of_speech}')
+        st.markdown(
+            f'Date first used: {list_of_word_variants[t].date}')
+        st.markdown(
+            f'{list_of_word_variants[t].etymology}')
 
 st.markdown(
     f'{list_of_word_variants[0].definition}')
@@ -16,6 +26,8 @@ st.markdown(
     f'Date first used: **{list_of_word_variants[0].date}**')
 # st.markdown(
 #     f'{list_of_word_variants[0].etymology}')
+if st.button("More Definitions"):
+    more_definitions()
 
 # st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
 # Open 'data/report.txt' for writing ('w')
