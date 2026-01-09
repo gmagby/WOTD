3e786236bada3b397b957d0ed324364fdb091ff0
gabriel <dvabnnzsngf>
2026-01-08 21:23:09 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -8,32 +8,31 @@ st.header("Word of the Day", divider="rainbow")
 st.subheader(wotd.WORD)
 
 def more_definitions():
-    for t in range (NUM):
+    for t in range (NUM-1):
         st.markdown(
             f'{list_of_word_variants[t].definition}')
         st.markdown(
             f'Type of Speech: **{list_of_word_variants[t].type_of_speech}**')
         st.markdown(
-            f'Date first used: **{list_of_word_variants[t].date}**')
+            f'Date first used: **{list_of_word_variants[t+1].date}**')
         # st.markdown(
         #     f'{list_of_word_variants[t].etymology}')
         st.header("",divider="rainbow")
 
-def first_definition(On=True):
-    if On:
-        st.markdown(
-            f'{list_of_word_variants[0].definition}')
-        st.markdown(
-            f'Type of speech: **{list_of_word_variants[0].type_of_speech}**')
-        st.markdown(
-            f'Date first used: **{list_of_word_variants[0].date}**')
-        # st.markdown(
-        #     f'{list_of_word_variants[0].etymology}')
+def first_definition():
+    st.markdown(
+        f'{list_of_word_variants[0].definition}')
+    st.markdown(
+        f'Type of speech: **{list_of_word_variants[0].type_of_speech}**')
+    st.markdown(
+        f'Date first used: **{list_of_word_variants[0].date}**')
+    # st.markdown(
+    #     f'{list_of_word_variants[0].etymology}')
 
-visible = first_definition()
+first_definition()
 
 if st.button("More Definitions"):
-    visible = more_definitions()
+    more_definitions()
 
 
 # st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
