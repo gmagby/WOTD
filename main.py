3e94b3e72c8a6e029c5a76567e4d4688774db208
gabriel <dvabnnzsngf>
2026-01-10 23:58:52 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def more_definitions():
 
 def first_definition():
     st.markdown(
-        "traveling from place to place")
+        "One who travels from place to place")
     st.markdown(
         f'Type of speech: **{list_of_word_variants[0].type_of_speech}**')
     st.markdown(
