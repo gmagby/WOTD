748a55e664caad89582fdcd68b23b70b2d84300c
gabriel <dvabnnzsngf>
2026-01-10 23:56:51 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def more_definitions():
 
 def first_definition():
     st.markdown(
-        f'{list_of_word_variants[0].definition}')
+        "traveling from place to place")
     st.markdown(
         f'Type of speech: **{list_of_word_variants[0].type_of_speech}**')
     st.markdown(
