9a3e93a2828d5c3069942fbc2cd8ff747324c117
gabriel <dvabnnzsngf>
2026-01-14 00:52:21 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -26,8 +26,11 @@ def first_definition():
         f'Type of speech: **{list_of_word_variants[0].type_of_speech}**')
     st.markdown(
         f'Date first used: **{list_of_word_variants[0].date}**')
-    # st.markdown(
-    #     f'{list_of_word_variants[0].etymology}')
+    st.markdown("late Middle English: from Old French contremander (verb), contremand (noun), from medieval Latin contramandare, from contra- ‘against’ + mandare ‘to order’."
+    )
+    st.markdown("countermand, mandate, command, mandatary, mandatory, and remand all come from this latin origin")
+    st.markdown("The Latin verb mandare means 'to order, commit to one's charge', literally 'to give into [one's] hand,' combining manus ('hand') and dare ('to give')")
+
 def instructions_app():
     st.markdown(
         '''Instructions on how to make WOTD into a widget on your homescreen.''')
