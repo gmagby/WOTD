cc2b443958b794b87b7593735988dd1d11f81600
gabriel <dvabnnzsngf>
2026-01-18 12:42:59 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ def first_definition():
         f'"characterized by or appealing to self-indulgence"')
     st.markdown(
             f'"marked by decay or decline"')
-    st.markdown(
-            f'"of or relating to a group of late 19th century French and English writers emphasizing artificial and unconventional subjects : of or relating to the decadents"')
+
     st.markdown(
         f'Type of speech: **{list_of_word_variants[0].type_of_speech}**')
     st.markdown(
