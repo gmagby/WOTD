08732f29fb20783081e643a8956b9688da9fd7a4
gabriel <dvabnnzsngf>
2026-01-19 00:54:51 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ def more_definitions():
 
 def first_definition():
     st.markdown(
-        f'"characterized by or appealing to self-indulgence"')
-    st.markdown(
-            f'"marked by decay or decline"')
+        f'Type of speech: **{list_of_word_variants[0].definition}**')
 
     st.markdown(
         f'Type of speech: **{list_of_word_variants[0].type_of_speech}**')
