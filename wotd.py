3a8934c51b01686f58a7d458ad2e28f02e8b5aa8
gabriel <dvabnnzsngf>
2026-01-19 21:54:54 -0600

    Added more definitions

--- wotd.py
+++ wotd.py
@@ -37,13 +37,9 @@ def get_response_dictionary(ref, word, key):
 def cleaner(clean_text):
     clean_text = str(clean_text)
     print(clean_text)
-    clean_text = re.sub(r",", ", \n ", clean_text)
-    clean_text = re.sub(r"{ds||||}", "", clean_text)
-    clean_text = re.sub(r"{dst1||]||}", "", clean_text)
-    clean_text = re.sub(r"dst1", " ", clean_text)
     clean_text = re.sub(r"[()\#[/@<>{}=~|?]", '', clean_text)
     # clean_text = re.sub(r"[^a-zA-Z0-9:]", " ", clean_text)
-    clean_text = re.sub(r"dst1", "", clean_text)
+    clean_text = re.sub(r"\s+", " ", clean_text).strip()  # Remove extra spaces
     print(clean_text)
     return clean_text
 
