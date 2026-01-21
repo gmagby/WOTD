f3e74386011424fb4982eb0d07be72c7cbff8229
gabriel <dvabnnzsngf>
2026-01-21 03:10:17 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def first_definition():
     # st.markdown(
     #     f'Type of speech: **{list_of_word_variants[FAVORED].type_of_speech}**')
     st.markdown(
-        f'Date first used: **{list_of_word_variants[FAVORED].date}**')
+        f'Date first used: {list_of_word_variants[FAVORED].date}')
 
 
 def more_definitions():
