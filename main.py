3a3ecf9b4bc0bae460c1f5cd40bf3843ed541e5c
gabriel <dvabnnzsngf>
2026-01-23 22:42:11 -0600

    Added more definitions

--- main.py
+++ main.py
@@ -73,7 +73,8 @@ example_img = Image.open(f'{wotd.WORD}.jpg')
 st.image(example_img)
 
 st.subheader("Click below to learn how others have used this word.")
-st.header('''https://www.merriam-webster.com/sentences/aver''')
+if st.button("Click below to learn how others have used this word."):
+    'https://www.merriam-webster.com/sentences/aver'
 
 
 # st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
