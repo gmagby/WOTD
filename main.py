9b91cc4d419f5e47b45382cfb7709a1dac9b216b
gabriel <dvabnnzsngf>
2026-01-27 00:24:08 -0600

    New WOTD

--- main.py
+++ main.py
@@ -81,18 +81,9 @@ def find_photo():
     try:
         example_img = Image.open(f'{wotd.WORD}.jpg')
     except FileNotFoundError:
-        example_img = Image.open(f'{wotd.WORD}.webp')
-
+        pass
+    example_img = Image.open(f'{wotd.WORD}.webp')
     st.image(example_img)
-
-
-
-# st.subheader("Click below to learn how others have used this word.")
-
-
-
-
-
 
 # st.image("https://images.unsplash.com/photo-1535930749574-1399327ce78f?q=80&w=1936&auto=format&fit=crop")
 # Open 'data/report.txt' for writing ('w')
