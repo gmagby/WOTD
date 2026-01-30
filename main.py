04ce757b2d0723d914aff8b947ae7a47723282bb
gabriel <dvabnnzsngf>
2026-01-29 23:37:26 -0600

    New WOTD

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ formated_definition = split_text(list_of_word_variants[favored].definition)
 
 def first_definition():
     st.markdown(
-        "one who is unimaginative, rigid, or overly concerned with minor details in the presentation or use of knowledge; sometimes, specifically : a person who adheres strictly to formal rules in teaching",)
+        "one who is unimaginative, rigid, or overly concerned with minor details in the presentation or use of knowledge; sometimes,  specifically : a person who adheres strictly to formal rules in teaching",)
     st.markdown(
         "one who makes a show of knowledge",)
     st.markdown(
