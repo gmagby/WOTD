1bd018033babca1435aec2491fdbcb0985e13cee
gabriel <dvabnnzsngf>
2026-02-06 22:43:26 -0600

    New WOTD

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ def first_definition():
         st.markdown(formated_definition[t])
     st.markdown(
         f'Date first used: {list_of_word_variants[favored].date}')
-    st.markdown(
-        f'Etymology:  {list_of_word_variants[favored].etymology}'
+    if list_of_word_variants[favored].etymology != 'No info available':
+        st.markdown(
+            f'Etymology:  {list_of_word_variants[favored].etymology}'
     )
 
 
