94c2fcd404ad9fe33ced114e9d80775f44f4aa51
gabriel <dvabnnzsngf>
2026-02-11 01:05:28 -0600

    New WOTD

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def more_definitions():
             pass
 
         else:
+            st.header(WORD, divider="rainbow")
             st.markdown(
                 f'{list_of_word_variants[t+1].definition}')
             st.markdown(
@@ -45,7 +46,7 @@ def more_definitions():
                 f'Date first used: {list_of_word_variants[t+1].date}')
             st.markdown(f'Synonyms: {list_of_word_variants[t+1].synonyms}')
             st.markdown(f'Antonyms: {list_of_word_variants[t + 1].antonyms}')
-            st.header("", divider="rainbow")
+
             # if list_of_word_variants[favored].etymology != 'No info available':
             #     for t in range(len(formated_etymology)):
             #         st.markdown(formated_etymology[t+1])
