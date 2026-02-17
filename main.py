564999f829c7fe2cd75f6e0ff7b025780c9fbd08
gabriel <dvabnnzsngf>
2026-02-17 00:25:37 -0600

    New WOTD

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ st.sidebar.markdown(f'**{list_of_word_variants[favored].type_of_speech}**')
 
 if list_of_word_variants[favored].etymology != 'No info available':
     if st.sidebar.button("Etymology"):
-        for t in range(num-1):
+        for t in range(len(formated_etymology)):
             st.sidebar.markdown(formated_etymology[t])
 
 if st.sidebar.button('Thesaurus'):
