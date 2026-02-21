e6e29b94c549f052e79aa30ebc69a0550ccc9a66
gabriel <dvabnnzsngf>
2026-02-20 23:55:30 -0600

    New WOTD

--- wotd.py
+++ wotd.py
@@ -111,7 +111,7 @@ def extract_synonyms(data, nyms):
     nyms_lists = []  # List to hold lists of synonyms/antonyms for each entry
 
     for entry in data:
-        entry_nyms_list = [syn for syn_group in entry['meta'].get(nyms, []) for syn in syn_group] or f'None found'
+        entry_nyms_list = [syn for syn_group in entry['meta'].get(nyms, []) for syn in syn_group] or [NONE_RESULT]
         nyms_lists.append(entry_nyms_list)  # Append the entry's list to the main list
         # nyms_lists = cleaner(nyms_lists)
         print(nyms_lists)
