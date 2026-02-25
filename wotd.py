a552be41c111d8ad36fc32111fb6b028a85c21f8
gabriel <dvabnnzsngf>
2026-02-25 03:55:12 -0600

    New WOTD

--- wotd.py
+++ wotd.py
@@ -2,7 +2,7 @@ import re
 import requests
 from streamlit.string_util import clean_text
 
-WORD = 'Elegiac'
+WORD = 'Elegy'
 REF_DICTIONARY = "collegiate"
 REF_THESAURUS = "thesaurus"
 DICTIONARY_KEY = 'f45f1248-4774-4d20-8d31-ecb2d70452e0'
