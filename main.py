5cb32029461e1a624fbe69e1321acda0a99921f4
gabriel <dvabnnzsngf>
2026-03-06 00:01:36 -0600

    New WOTD

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ def more_definitions():
         st.markdown(
             f'Date first used: {list_of_word_variants[t+1].date}')
         st.markdown(f'Synonyms: {list_of_word_variants[t+1].synonyms}')
-        st.markdown(f'Antonyms: {list_of_word_variants[t + 1].antonyms}')
+        st.markdown(f'Antonyms: None found')
+        # st.markdown(f'Antonyms: {list_of_word_variants[t + 1].antonyms}')
 
 
 def instructions_app():
