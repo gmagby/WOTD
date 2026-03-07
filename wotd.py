4e0abbd1608b97e944b00ea0dcf7ae59a1c165db
gabriel <dvabnnzsngf>
2026-03-07 13:25:12 -0600

    New WOTD

--- wotd.py
+++ wotd.py
@@ -211,11 +211,12 @@ def list_of_prev_wotd_cleaner(clean_text):
     clean_text = re.sub(r"]", '', clean_text)
     clean_text = re.sub(r"'", '', clean_text)
     clean_text = re.sub(r"2", '', clean_text)
-
-    print(clean_text)
-    return clean_text
+    clean_list = clean_text.split(", ")
+    clean_list.sort()
+    print(clean_list)
+    return clean_list
 
 # Example usage
 photo_folder = r"Photos"
-previous_WOTD = list_of_prev_wotd_cleaner(list_photo_names(photo_folder).sort())
+previous_WOTD = list_of_prev_wotd_cleaner(list_photo_names(photo_folder))
 
