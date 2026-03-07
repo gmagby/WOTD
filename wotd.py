8e34b8fbbb047790c9a8e8793a14f2cf59fbb2cc
gabriel <dvabnnzsngf>
2026-03-06 23:17:04 -0600

    New WOTD

--- wotd.py
+++ wotd.py
@@ -207,6 +207,8 @@ def list_cleaner(clean_text):
     clean_text = re.sub(r'.gif', '', clean_text)
     clean_text = re.sub(r'.webp', '', clean_text)
     clean_text = re.sub(r'.avif', '', clean_text)
+    clean_text = re.sub(r"[\#[/@<>{}=~|?]", '', clean_text)
+    clean_text = re.sub(r"]", '', clean_text)
     print(clean_text)
     clean_list = clean_text.split(", ")
     print(clean_list)
