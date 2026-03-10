21d3229318bfbe006d8f94a3c2485804f2bd17b0
gabriel <dvabnnzsngf>
2026-03-10 01:05:53 -0500

    New WOTD

--- main.py
+++ main.py
@@ -85,12 +85,12 @@ def guide_func():
     else:
         pass
 
-    if check_for_no_data(list_of_word_variants[favored].synonyms):
-        if st.sidebar.button('Thesaurus'):
-            st.sidebar.markdown("Synonyms:")
-            st.sidebar.markdown(list_of_word_variants[favored].synonyms)
-            st.sidebar.markdown("Antonyms:")
-            st.sidebar.markdown(list_of_word_variants[favored].antonyms)
+
+    if st.sidebar.button('Thesaurus'):
+        st.sidebar.markdown("Synonyms:")
+        st.sidebar.markdown(list_of_word_variants[favored].synonyms)
+        st.sidebar.markdown("Antonyms:")
+        st.sidebar.markdown(list_of_word_variants[favored].antonyms)
     else:
         pass
 
