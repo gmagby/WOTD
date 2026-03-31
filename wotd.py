c615e1949706f7b8bcadae39f9325008f9ff69c7
gabriel <dvabnnzsngf>
2026-03-31 00:14:43 -0500

    New WOTD

--- wotd.py
+++ wotd.py
@@ -89,7 +89,6 @@ def cleaner(clean_text, sharp=None):
         clean_text = re.sub(r"]", '', clean_text)
         clean_text = re.sub(r" u ", " 'u' ", clean_text)
         clean_text = re.sub(r"'", '', clean_text)
-        clean_text = re.sub(r"'[[]] '", '', clean_text)
         return clean_text
     if sharp == 1:  # Definition cleaner
         clean_text = base_cleaner(definition_cleaner(clean_text))
