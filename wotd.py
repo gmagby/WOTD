f5bbd888e615cf7105099b7793a6cc005db9ea58
gabriel <dvabnnzsngf>
2026-04-08 01:01:26 -0500

    New WOTD

--- wotd.py
+++ wotd.py
@@ -4,11 +4,11 @@ import os
 from cleaner_file import cleaner
 from cleaner_file import list_of_prev_wotd_cleaner
 
-WORD = 'winnow'
+WORD = 'skulduggery'
 REF_DICTIONARY = "collegiate"
 REF_THESAURUS = "thesaurus"
 DICTIONARY_KEY = 'f45f1248-4774-4d20-8d31-ecb2d70452e0'
-Thesaurus_key = '2431331e-690c-4d83-96ac-1f4e9cb350d5'
+THESAURUS_KEY = '2431331e-690c-4d83-96ac-1f4e9cb350d5'
 DEFINITION_KEY = 'shortdef'
 TYPE_OF_SPEECH_KEY = 'fl'
 DATE_KEY = 'date'
@@ -19,23 +19,21 @@ NONE_RESULT = 'No info available'
 TXT_FOLDER = r'txt_files'
 THESAURUS_FOLDER = r'Thesaurus'
 WOTD_ARCHIVE = r'Former Words.txt'
-ARCHIVE_PATH = r"other_files/Former Words"
 PHOTO_FOLDER = r"Photos"
 OTHER_FILES = r"other_files"
+ARCHIVE_PATH = r'other_files/Former Words.txt'
 
-def get_response_dictionary(ref, word, key):
+def get_response(ref, word, key):
     url = f"https://www.dictionaryapi.com/api/v3/references/{ref}/json/{word}?key={key}"
     response = requests.get(url)
     print(url)
     return response.json()
 
 def get_data(word_selected):
-    data = get_response_dictionary(REF_DICTIONARY, word_selected, DICTIONARY_KEY)
-    return data
+    return get_response(REF_DICTIONARY, word_selected, DICTIONARY_KEY)
 
 def get_thes_data(word_selected):
-    thes_data = get_response_dictionary(REF_THESAURUS, word_selected, Thesaurus_key)
-    return thes_data
+    return get_response(REF_THESAURUS, word_selected, THESAURUS_KEY)
 
 def check_for_no_data(text):
     if text != 'No info available':
@@ -54,32 +52,17 @@ def extract_synonyms(data, nyms):
     for entry in data:
         try:
             syn_group = entry['meta'].get(nyms, [])
-            synonyms.append([syn for syn in syn_group])
+            synonyms.append(syn_group)
         except (KeyError, TypeError):
-            synonyms.append(NONE_RESULT)  # Append an empty list if there's an error
+            synonyms.append(NONE_RESULT)
     return synonyms
 
-def create_file(folder, chosen_word):
+def create_file(folder, chosen_word, is_thesaurus=False):
     file_name = add_txt_to_file_name(chosen_word)
-    try:
-        folder_path = os.path.join(folder, file_name)
-        if os.path.exists(folder_path):
-            pass
-        else:
-            save_new_data(folder_path, get_data(chosen_word))
-    except ValueError:
-        print("Error", "Something went wrong.")
-
-def create_thes_file(folder, chosen_word):
-    file_name = add_txt_to_file_name(chosen_word)
-    try:
-        folder_path = os.path.join(folder, file_name)
-        if os.path.exists(folder_path):
-            pass
-        else:
-            save_new_data(folder_path, get_thes_data(chosen_word))
-    except ValueError:
-        print("Error", "Something went wrong.")
+    folder_path = os.path.join(folder, file_name)
+    if not os.path.exists(folder_path):
+        data = get_thes_data(chosen_word) if is_thesaurus else get_data(chosen_word)
+        save_new_data(folder_path, data)
 
 def save_new_data(file_name, data):
     with open(file_name, "w") as f:
@@ -96,20 +79,13 @@ def read_data(path):
         print("Error", "Something went wrong.")
 
 def create_folder_path(folder_name, file_name):
-    file_name = add_txt_to_file_name(file_name)
-    folder_path = os.path.join(folder_name, file_name)
-    return folder_path
+    return os.path.join(folder_name, add_txt_to_file_name(file_name))
 
 def find_data_with_path(folder_name, file_name):
-    new_path = create_folder_path(folder_name, file_name)
-    data = read_data(new_path)
-    return data
+    return read_data(create_folder_path(folder_name, file_name))
 
 def add_txt_to_file_name(text):
-    file_name = f'{text}.txt'
-    file_name = cleaner(file_name, 5)
-    file_name = cleaner(file_name, 5)
-    return file_name
+    return f'{cleaner(text, 5)}.txt'
 
 def list_photo_names(folder_path):
     return [file for file in os.listdir(folder_path) if
@@ -145,16 +121,19 @@ def create_variants(word_selected):
     variants = create_word_variants(definition_list, date_list, etymology_list, type_of_speech_list, synonyms_list, antonyms_list)
     return variants
 
+
 def add_new_word(chosen_word):
-    previous_WOTD = read_data(ARCHIVE_PATH)
-    if chosen_word is not previous_WOTD:
-        previous_WOTD.append(WORD)
+    previous_WOTD = list_of_prev_wotd_cleaner(list_photo_names)
+    if chosen_word in previous_WOTD:
+        print("Word already added")
+        pass
+    else:
+        previous_WOTD.append(chosen_word)  # Use chosen_word instead of WORD
         save_new_data(ARCHIVE_PATH, previous_WOTD)
-    return previous_WOTD
 
 def create_archive(chosen_word):
     create_file(TXT_FOLDER, chosen_word)
-    create_thes_file(THESAURUS_FOLDER, chosen_word)
+    create_file(THESAURUS_FOLDER, chosen_word, is_thesaurus=True)
 
 # Text to List Converter
 def format_text(text):
@@ -181,5 +160,5 @@ def main():
     first_definition()
 
 main()
-previous_WOTD = read_data(ARCHIVE_PATH)
+previous_WOTD = list_of_prev_wotd_cleaner(list_photo_names)
 print(previous_WOTD)
