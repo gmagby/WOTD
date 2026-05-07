ebab497df853fcae054a0efa4c18105f46f3ff53
gabriel <dvabnnzsngf>
2026-05-07 00:37:02 -0500

    New WOTD

--- main.py
+++ main.py
@@ -105,13 +105,12 @@ def sidebar(chosen_word, variant):
                 st.sidebar.markdown(variant[favored].etymology)
     else:
         pass
+
     if st.sidebar.button('Thesaurus'):
         if check_for_no_data(variant[favored].synonyms[0]):
             st.sidebar.markdown("Synonyms:")
             st.sidebar.markdown(", ".join(variant[favored].synonyms[0]))
-        else:
-            pass
-        if check_for_no_data(variant[favored].antonyms[0]):
+        elif check_for_no_data(variant[favored].antonyms[0]):
             st.sidebar.markdown("Antonyms:")
             st.sidebar.markdown(", ".join(variant[favored].antonyms[0]))
         else:
