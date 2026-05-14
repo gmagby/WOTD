3d70f9fb9e1db57c125507b92e7f16b1652b8f9d
gabriel <dvabnnzsngf>
2026-05-14 11:15:31 -0500

    New WOTD

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def check_for_nyms(nym, text):
 
 def markup_nyms(variant, iter):
     check_for_nyms(variant[iter].synonyms[0], "Synonyms:")
-    check_for_nyms(variant[iter].antonyms[0], "Antonyms:")
+    # check_for_nyms(variant[iter].antonyms[0], "Antonyms:")
 
 def display_instructions():
     st.sidebar.markdown('Instructions on how to make WOTD into a widget on your homescreen.')
